d4292c92473d0488fc2d050a0a513a6e04b0e64b
jtoshiro <joshiro@telus.net>
2024-07-25 23:53:03 -0400

    incorporate error handling

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -34,4 +34,11 @@ def emotion_detector(text_to_analyze):
             'dominant_emotion': dominant_emotion
         }
     else:
-        return None
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
