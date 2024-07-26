694bd3329f0a6afc8e11acc3ff8669390c202386
jtoshiro <joshiro@telus.net>
2024-07-25 21:19:12 -0400

    Initial Commit

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,37 @@
+import requests # Import the requests library to handle HTTP requests
+import json
+
+# Define a function named emotion_detector that takes a string input (text_to_analyse)
+def emotion_detector(text_to_analyze):
+    
+    # URL of the emotion predict service
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    
+    # Custom header specifying the model ID for the emotion detector service
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    
+    # Constructing the request payload in the expected format
+    myobj = {"raw_document": {"text": text_to_analyze}}
+
+    # Send a POST request to the API with the text and headers
+    response = requests.post(url, json=myobj, headers=header)
+
+    # Send a POST request to the API with the text and headers
+    formatted_response = json.loads(response.text)
+
+    # Extracting emotion label from the response
+    # I couldn't get this to works so I had to use Vandana's code
+    if response.status_code == 200:
+        emotion_predictions = formatted_response['emotionPredictions'][0]['emotion']
+        dominant_emotion = max(emotion_predictions.items(), key=lambda x: x[1])[0]
+
+        return {
+            'anger': emotion_predictions['anger'],
+            'disgust': emotion_predictions['disgust'],
+            'fear': emotion_predictions['fear'],
+            'joy': emotion_predictions['joy'],
+            'sadness': emotion_predictions['sadness'],
+            'dominant_emotion': dominant_emotion
+        }
+    else:
+        return None
