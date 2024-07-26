01a29414abc19b80cbbd85af5df52567498708f8
jtoshiro <joshiro@telus.net>
2024-07-26 00:36:12 -0400

    final changes after static code analysis

--- server.py
+++ server.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template, request
-from EmotionDetection.emotion_detection import emotion_detector
+""" Import functions """
+from flask import Flask, render_template, request # Import functions
+from EmotionDetection.emotion_detection import emotion_detector # Import functions
 
-app = Flask("Emotion Detector")
+app = Flask("Emotion Detector") # Initiate application
 
 @app.route("/emotionDetector")
 def get_emotion_details():
+    """ Define the function """
     # Retrieve the text to analyze from the request arguments
     text_to_analyze = request.args.get('textToAnalyze')
 
@@ -14,12 +16,14 @@ def get_emotion_details():
     # Check if dominant emotion is None, indicating an error or invalid input
     if response['dominant_emotion'] is None:
         return "Invalid input! Try again."
-    else:
-        # Return a formatted string with the emotion values and dominant emotion
-        return f"For the given statement, the system response is 'anger': {response['anger']}, 'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['joy']} and 'sadness': {response['sadness']}. The dominant emotion is {response['dominant_emotion']}."
+    # Return a formatted string with the emotion values and dominant emotion
+    return f"For the given statement, the system response is 'anger': {response['anger']}, \
+    'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['joy']} and \
+    'sadness': {response['sadness']}. The dominant emotion is {response['dominant_emotion']}."
 
 @app.route("/")
 def render_index_page():
+    """ Render the HTML template """
     return render_template('index.html')
 
 if __name__ == "__main__":
